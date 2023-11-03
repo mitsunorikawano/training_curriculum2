a7b9afed2422b89659602937e0f340eca6add6d9
mitsunorikawano <mitsunori94@yahoo.co.jp>
2023-11-04 00:26:00 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,7 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    binding.pry
+
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -31,13 +31,26 @@ class CalendarsController < ApplicationController
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
+    # ７回繰返しを行う
     7.times do |x|
-
       today_plans = []
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
+         # xは0からスタートして繰り返すごとに値を1つ繰り上げる０,1,2,3
       end
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
+
+      # 
+      wday_num = Date.today.wday + x # wdayメソッドを用いて取得した数値
+
+      if wday_num >= 7
+      wday_num = wday_num -7 
+      else
+      wday_num = wday_num
+
+      end
+
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans ,:wday => wdays[wday_num]}
+      
       @week_days.push(days)
     end
 
